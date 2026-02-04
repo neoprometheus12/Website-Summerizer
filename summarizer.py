21a7c06d1bd5363ad677c400ae8b399a023a38a4
neoprometheus12 <ratul.sikder.iitkgp@gmail.com>
2026-02-04 23:20:40 +0530

    summarizer.py

--- summarizer.py
+++ summarizer.py
@@ -23,13 +23,13 @@ else:
 
 SYSTEM_PROMPT = """
 You are a snarky assistant that analyzes the contents of a website,
-and provides a short, snarky, humorous summary, ignoring text that might be navigation related.
+and provides a tabulated, snarky, exciting elaborated summary, ignoring text that might be navigation related.
 Respond in markdown. Do not wrap the markdown in a code block - respond just with the markdown.
 """
 
 USER_PROMPT_PREFIX = """
 Here are the contents of a website.
-Provide a short summary of this website.
+Provide a about of this website.
 If it includes news or announcements, then summarize these too.
 """
 
